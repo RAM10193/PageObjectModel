95d1e646369d5e9ddf101a3e7d1d21c45229b117
RAM10193 <gkmram@gmail.com>
2022-06-17 03:52:11 +0530

    Add project files.

---
+++ PageAutomation/Testcase/PageAutomationTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using PageAutomation.SupportFns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageAutomation
+{
+    class Program
+    {
+      [SetUp]
+      public void initialize()
+        {
+            driverInit.driver.Navigate().GoToUrl("https://www.orangehrm.com/open-source/open-source-on-demand");
+            driverInit.driver.Manage().Window.Maximize();
+        }
+
+    [Test]
+    public void Loginfunction()
+        {
+
+        }
+
+    [TearDown]
+    public void closebrowser()
+        {
+
+        }   
+    }
+}
